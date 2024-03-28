d1228e0934d217eee570c65bac4b6a39ae01eb65
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-03-28 07:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,21 +11,65 @@ function convertToRoman(num) {
 
   //your code here
 
-    let num = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
-    let sym = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
-    let i=12;
-	let ans = "";
-    while(number>0)
-    {
-    let div = Math.floor(number/num[i]);
-    number = number%num[i];
-    while(div--)
-    {
-        ans += (sym[i]);
+ //    let num = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+ //    let sym = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+ //    let i=12;
+	// let ans = "";
+ //    while(number>0)
+ //    {
+ //    let div = Math.floor(number/num[i]);
+ //    number = number%num[i];
+ //    while(div--)
+ //    {
+ //        ans += (sym[i]);
+ //    }
+ //    i--;
+ //    }
+	// return ans;
+
+	let romanNumeral = ""
+
+  while (num !== 0) {
+    if (num >= 1000) {
+      romanNumeral += "M"
+      num -= 1000
+    } else if (num >= 500) {
+        romanNumeral += "D"
+        num -= 500
+    } else if (num >= 400) {
+        romanNumeral += "CD"
+        num -= 400
+    } else if (num >= 100) {
+        romanNumeral += "C"
+        num -= 100
+    } else if (num >= 90) {
+        romanNumeral += "XC"
+        num -= 90
+    } else if (num >= 50) {
+        romanNumeral += "L"
+        num -= 50
+    } else if (num >= 40) {
+        romanNumeral += "XL"
+        num -= 40
+    } else if (num >= 10) {
+        romanNumeral += "X"
+        num -= 10
+    } else if (num >= 9) {
+        romanNumeral += "IX"
+        num -= 9
+    } else if (num >= 5) {
+        romanNumeral += "V"
+        num -= 5
+    } else if (num >= 4) {
+        romanNumeral += "IV"
+        num -= 4
+    } else if (num >= 1) {
+        romanNumeral += "I"
+        num -= 1
     }
-    i--;
-    }
-	return ans;
+  }
+
+  return romanNumeral
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
