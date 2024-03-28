e1b9e0c0806377d9ea17e43c5f85824835a09aed
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-03-28 06:11:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,16 +11,27 @@ function convertToRoman(num) {
 
   //your code here
 
-    let result = '';
-
-    for (let key in obj) {
-        while (num >= obj[key][1]) {
-            result += obj[key][0];
-            num -= obj[key][1];
-        }
-    }
-
-    return result;
+    let m = ["", "M", "MM", "MMM"];
+    let c = ["", "C", "CC", "CCC", "CD", "D",
+                        "DC", "DCC", "DCCC", "CM"];
+    let x = ["", "X", "XX", "XXX", "XL", "L",
+                        "LX", "LXX", "LXXX", "XC"];
+    let i = ["", "I", "II", "III", "IV", "V",
+                        "VI", "VII", "VIII", "IX"];
+         
+    // Converting to roman
+    let a1 = Math.floor(num/1000);
+    let a2 = Math.floor((num%1000)/100);
+    let a3 = Math.floor((num%100)/10);
+ 
+    let thousands = m[a1];
+    let hundreds = c[a2];
+    let tens = x[a3];
+    let ones = i[num%10];
+         
+    let ans = thousands + hundreds + tens + ones;
+         
+    return ans;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
